8971bdd8e4ce95f02adc5e7048dbbb3bdf3880b6
Mateusz Gasior <mr.nimelo@gmail.com>
2026-01-08 21:25:18 +0000

    improved example in windowsx

--- windowsx/example/main.go
+++ windowsx/example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/nimelo/tui-go/windowsx"
 	"golang.org/x/sys/windows"
@@ -31,7 +32,7 @@ func main() {
 		if record.EventType == windowsx.KEY_EVENT {
 			keyEvent := record.KeyEvent()
 
-			if keyEvent.Char[0] == 'q' {
+			if keyEvent.Char == 'q' {
 				break
 			}
 		}
@@ -41,22 +42,90 @@ func main() {
 func stringify(r windowsx.INPUT_RECORD) string {
 	switch r.EventType {
 	case windowsx.KEY_EVENT:
-		k := r.KeyEvent()
-		state := "UP"
-		if k.KeyDown != 0 {
-			state = "DOWN"
-		}
-		return fmt.Sprintf("KEY_EVENT: [%s] VK: 0x%02X, Char: %c", state, k.VirtualKeyCode, k.Char[0])
-
+		e := r.KeyEvent()
+		return stringifyKey(e)
+	case windowsx.FOCUS_EVENT:
+		e := r.FocusEvent()
+		return stringifyFocus(e)
 	case windowsx.MOUSE_EVENT:
 		m := r.MouseEvent()
-		return fmt.Sprintf("MOUSE_EVENT: Pos(%d, %d), Buttons: 0x%X", m.MousePosition.X, m.MousePosition.Y, m.ButtonState)
-
+		return stringifyMouse(m)
 	case windowsx.WINDOW_BUFFER_SIZE_EVENT:
 		s := r.WindowsBufferSizeEvent()
-		return fmt.Sprintf("RESIZE: %dx%d", s.Size.X, s.Size.Y)
-
+		return stringifyWindow(s)
 	default:
 		return fmt.Sprintf("EVENT_TYPE: %d", r.EventType)
 	}
+}
+
+func stringifyKey(e *windowsx.KEY_EVENT_RECORD) string {
+	state := "UP"
+	if e.KeyDown != 0 {
+		state = "DOWN"
+	}
+
+	source := "NATIVE"
+	if e.VirtualKeyCode == 0 {
+		source = "SYNTH"
+	}
+
+	vk, ok := windowsx.VirtualKeyMap[e.VirtualKeyCode]
+	if !ok {
+		vk = "-"
+	}
+
+	var sb strings.Builder
+
+	sb.WriteString("KEY_EVENT: ")
+	sb.WriteString(fmt.Sprintf("[%s] ", state))
+	sb.WriteString(fmt.Sprintf("[%s] ", source))
+	sb.WriteString(fmt.Sprintf("VK: %s (0x%02X) ", vk, e.VirtualKeyCode))
+	sb.WriteString(fmt.Sprintf("ControlKey: %s ", stringifyControlKey(e.ControlKeyState)))
+	if e.Char >= 32 && e.Char <= 126 {
+		sb.WriteString(fmt.Sprintf("Char: '%c' (0x%02X) ", e.Char, e.Char))
+	} else {
+		sb.WriteString(fmt.Sprintf("Char: [CTRL](0x%02X) ", e.Char))
+	}
+	sb.WriteString(fmt.Sprintf("Count: %d ", e.RepeatCount))
+
+	return sb.String()
+}
+
+func stringifyControlKey(state uint32) string {
+	var sb strings.Builder
+
+	first := true
+	for k, v := range windowsx.ControlKeyStateMap {
+		if state&k != 0 {
+			if !first {
+				sb.WriteString(" | ")
+			}
+			first = false
+
+			sb.WriteString(v)
+		}
+	}
+
+	if sb.Len() == 0 {
+		return "-"
+	}
+
+	return sb.String()
+}
+
+func stringifyWindow(s *windowsx.WINDOW_BUFFER_SIZE_RECORD) string {
+	return fmt.Sprintf("RESIZE: %dx%d", s.Size.X, s.Size.Y)
+}
+
+func stringifyMouse(m *windowsx.MOUSE_EVENT_RECORD) string {
+	return fmt.Sprintf("MOUSE_EVENT: Pos(%d, %d), Buttons: 0x%X", m.MousePosition.X, m.MousePosition.Y, m.ButtonState)
+}
+
+func stringifyFocus(e *windowsx.FOCUS_EVENT_RECORD) string {
+	focus := "ENTER"
+	if e.SetFocus == 0 {
+		focus = "EXIT"
+	}
+
+	return fmt.Sprintf("FOCUS_EVENT: [%s]", focus)
 }
--- windowsx/types.go
+++ windowsx/types.go
@@ -38,6 +38,18 @@ const (
 	SHIFT_PRESSED      = 0x0010
 )
 
+var ControlKeyStateMap = map[uint32]string{
+	CAPSLOCK_ON:        "CAPSLOCK_ON",
+	ENHANCED_KEY:       "ENHANCED_KEY",
+	LEFT_ALT_PRESSED:   "LEFT_ALT_PRESSED",
+	LEFT_CTRL_PRESSED:  "LEFT_CTRL_PRESSED",
+	NUMLOCK_ON:         "NUMLOCK_ON",
+	RIGHT_ALT_PRESSED:  "RIGHT_ALT_PRESSED",
+	RIGHT_CTRL_PRESSED: "RIGHT_CTRL_PRESSED",
+	SCROLLLOCK_ON:      "SCROLLLOCK_ON",
+	SHIFT_PRESSED:      "SHIFT_PRESSED",
+}
+
 // See https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 const (
 	VK_LBUTTON                         = 0x01
@@ -84,6 +96,42 @@ const (
 	VK_INSERT                          = 0x2D
 	VK_DELETE                          = 0x2E
 	VK_HELP                            = 0x2F
+	VK_0                               = 0x30
+	VK_1                               = 0x31
+	VK_2                               = 0x32
+	VK_3                               = 0x33
+	VK_4                               = 0x34
+	VK_5                               = 0x35
+	VK_6                               = 0x36
+	VK_7                               = 0x37
+	VK_8                               = 0x38
+	VK_9                               = 0x39
+	VK_A                               = 0x41
+	VK_B                               = 0x42
+	VK_C                               = 0x43
+	VK_D                               = 0x44
+	VK_E                               = 0x45
+	VK_F                               = 0x46
+	VK_G                               = 0x47
+	VK_H                               = 0x48
+	VK_I                               = 0x49
+	VK_J                               = 0x4A
+	VK_K                               = 0x4B
+	VK_L                               = 0x4C
+	VK_M                               = 0x4D
+	VK_N                               = 0x4E
+	VK_O                               = 0x4F
+	VK_P                               = 0x50
+	VK_Q                               = 0x51
+	VK_R                               = 0x52
+	VK_S                               = 0x53
+	VK_T                               = 0x54
+	VK_U                               = 0x55
+	VK_V                               = 0x56
+	VK_W                               = 0x57
+	VK_X                               = 0x58
+	VK_Y                               = 0x59
+	VK_Z                               = 0x5A
 	VK_LWIN                            = 0x5B
 	VK_RWIN                            = 0x5C
 	VK_APPS                            = 0x5D
@@ -204,13 +252,212 @@ const (
 	VK_OEM_CLEAR                       = 0xFE
 )
 
+var VirtualKeyMap = map[uint16]string{
+	VK_LBUTTON:                         "VK_LBUTTON",
+	VK_RBUTTON:                         "VK_RBUTTON",
+	VK_CANCEL:                          "VK_CANCEL",
+	VK_MBUTTON:                         "VK_MBUTTON",
+	VK_XBUTTON1:                        "VK_XBUTTON1",
+	VK_XBUTTON2:                        "VK_XBUTTON2",
+	VK_BACK:                            "VK_BACK",
+	VK_TAB:                             "VK_TAB",
+	VK_CLEAR:                           "VK_CLEAR",
+	VK_RETURN:                          "VK_RETURN",
+	VK_SHIFT:                           "VK_SHIFT",
+	VK_CONTROL:                         "VK_CONTROL",
+	VK_MENU:                            "VK_MENU",
+	VK_PAUSE:                           "VK_PAUSE",
+	VK_CAPITAL:                         "VK_CAPITAL",
+	VK_HANGUL:                          "VK_HANGUL|VK_KANA",
+	VK_IME_ON:                          "VK_IME_ON",
+	VK_JUNJA:                           "VK_JUNJA",
+	VK_FINAL:                           "VK_FINAL",
+	VK_HANJA:                           "VK_HANJA|VK_KANJI",
+	VK_IME_OFF:                         "VK_IME_OFF",
+	VK_ESCAPE:                          "VK_ESCAPE",
+	VK_CONVERT:                         "VK_CONVERT",
+	VK_NONCONVERT:                      "VK_NONCONVERT",
+	VK_ACCEPT:                          "VK_ACCEPT",
+	VK_MODECHANGE:                      "VK_MODECHANGE",
+	VK_SPACE:                           "VK_SPACE",
+	VK_PRIOR:                           "VK_PRIOR",
+	VK_NEXT:                            "VK_NEXT",
+	VK_END:                             "VK_END",
+	VK_HOME:                            "VK_HOME",
+	VK_LEFT:                            "VK_LEFT",
+	VK_UP:                              "VK_UP",
+	VK_RIGHT:                           "VK_RIGHT",
+	VK_DOWN:                            "VK_DOWN",
+	VK_SELECT:                          "VK_SELECT",
+	VK_PRINT:                           "VK_PRINT",
+	VK_EXECUTE:                         "VK_EXECUTE",
+	VK_SNAPSHOT:                        "VK_SNAPSHOT",
+	VK_INSERT:                          "VK_INSERT",
+	VK_DELETE:                          "VK_DELETE",
+	VK_HELP:                            "VK_HELP",
+	VK_0:                               "VK_0",
+	VK_1:                               "VK_1",
+	VK_2:                               "VK_2",
+	VK_3:                               "VK_3",
+	VK_4:                               "VK_4",
+	VK_5:                               "VK_5",
+	VK_6:                               "VK_6",
+	VK_7:                               "VK_7",
+	VK_8:                               "VK_8",
+	VK_9:                               "VK_9",
+	VK_A:                               "VK_A",
+	VK_B:                               "VK_B",
+	VK_C:                               "VK_C",
+	VK_D:                               "VK_D",
+	VK_E:                               "VK_E",
+	VK_F:                               "VK_F",
+	VK_G:                               "VK_G",
+	VK_H:                               "VK_H",
+	VK_I:                               "VK_I",
+	VK_J:                               "VK_J",
+	VK_K:                               "VK_K",
+	VK_L:                               "VK_L",
+	VK_M:                               "VK_M",
+	VK_N:                               "VK_N",
+	VK_O:                               "VK_O",
+	VK_P:                               "VK_P",
+	VK_Q:                               "VK_Q",
+	VK_R:                               "VK_R",
+	VK_S:                               "VK_S",
+	VK_T:                               "VK_T",
+	VK_U:                               "VK_U",
+	VK_V:                               "VK_V",
+	VK_W:                               "VK_W",
+	VK_X:                               "VK_X",
+	VK_Y:                               "VK_Y",
+	VK_Z:                               "VK_Z",
+	VK_LWIN:                            "VK_LWIN",
+	VK_RWIN:                            "VK_RWIN",
+	VK_APPS:                            "VK_APPS",
+	VK_SLEEP:                           "VK_SLEEP",
+	VK_NUMPAD0:                         "VK_NUMPAD0",
+	VK_NUMPAD1:                         "VK_NUMPAD1",
+	VK_NUMPAD2:                         "VK_NUMPAD2",
+	VK_NUMPAD3:                         "VK_NUMPAD3",
+	VK_NUMPAD4:                         "VK_NUMPAD4",
+	VK_NUMPAD5:                         "VK_NUMPAD5",
+	VK_NUMPAD6:                         "VK_NUMPAD6",
+	VK_NUMPAD7:                         "VK_NUMPAD7",
+	VK_NUMPAD8:                         "VK_NUMPAD8",
+	VK_NUMPAD9:                         "VK_NUMPAD9",
+	VK_MULTIPLY:                        "VK_MULTIPLY",
+	VK_ADD:                             "VK_ADD",
+	VK_SEPARATOR:                       "VK_SEPARATOR",
+	VK_SUBTRACT:                        "VK_SUBTRACT",
+	VK_DECIMAL:                         "VK_DECIMAL",
+	VK_DIVIDE:                          "VK_DIVIDE",
+	VK_F1:                              "VK_F1",
+	VK_F2:                              "VK_F2",
+	VK_F3:                              "VK_F3",
+	VK_F4:                              "VK_F4",
+	VK_F5:                              "VK_F5",
+	VK_F6:                              "VK_F6",
+	VK_F7:                              "VK_F7",
+	VK_F8:                              "VK_F8",
+	VK_F9:                              "VK_F9",
+	VK_F10:                             "VK_F10",
+	VK_F11:                             "VK_F11",
+	VK_F12:                             "VK_F12",
+	VK_F13:                             "VK_F13",
+	VK_F14:                             "VK_F14",
+	VK_F15:                             "VK_F15",
+	VK_F16:                             "VK_F16",
+	VK_F17:                             "VK_F17",
+	VK_F18:                             "VK_F18",
+	VK_F19:                             "VK_F19",
+	VK_F20:                             "VK_F20",
+	VK_F21:                             "VK_F21",
+	VK_F22:                             "VK_F22",
+	VK_F23:                             "VK_F23",
+	VK_F24:                             "VK_F24",
+	VK_NUMLOCK:                         "VK_NUMLOCK",
+	VK_SCROLL:                          "VK_SCROLL",
+	VK_LSHIFT:                          "VK_LSHIFT",
+	VK_RSHIFT:                          "VK_RSHIFT",
+	VK_LCONTROL:                        "VK_LCONTROL",
+	VK_RCONTROL:                        "VK_RCONTROL",
+	VK_LMENU:                           "VK_LMENU",
+	VK_RMENU:                           "VK_RMENU",
+	VK_BROWSER_BACK:                    "VK_BROWSER_BACK",
+	VK_BROWSER_FORWARD:                 "VK_BROWSER_FORWARD",
+	VK_BROWSER_REFRESH:                 "VK_BROWSER_REFRESH",
+	VK_BROWSER_STOP:                    "VK_BROWSER_STOP",
+	VK_BROWSER_SEARCH:                  "VK_BROWSER_SEARCH",
+	VK_BROWSER_FAVORITES:               "VK_BROWSER_FAVORITES",
+	VK_BROWSER_HOME:                    "VK_BROWSER_HOME",
+	VK_VOLUME_MUTE:                     "VK_VOLUME_MUTE",
+	VK_VOLUME_DOWN:                     "VK_VOLUME_DOWN",
+	VK_VOLUME_UP:                       "VK_VOLUME_UP",
+	VK_MEDIA_NEXT_TRACK:                "VK_MEDIA_NEXT_TRACK",
+	VK_MEDIA_PREV_TRACK:                "VK_MEDIA_PREV_TRACK",
+	VK_MEDIA_STOP:                      "VK_MEDIA_STOP",
+	VK_MEDIA_PLAY_PAUSE:                "VK_MEDIA_PLAY_PAUSE",
+	VK_LAUNCH_MAIL:                     "VK_LAUNCH_MAIL",
+	VK_LAUNCH_MEDIA_SELECT:             "VK_LAUNCH_MEDIA_SELECT",
+	VK_LAUNCH_APP1:                     "VK_LAUNCH_APP1",
+	VK_LAUNCH_APP2:                     "VK_LAUNCH_APP2",
+	VK_OEM_1:                           "VK_OEM_1",
+	VK_OEM_PLUS:                        "VK_OEM_PLUS",
+	VK_OEM_COMMA:                       "VK_OEM_COMMA",
+	VK_OEM_MINUS:                       "VK_OEM_MINUS",
+	VK_OEM_PERIOD:                      "VK_OEM_PERIOD",
+	VK_OEM_2:                           "VK_OEM_2",
+	VK_OEM_3:                           "VK_OEM_3",
+	VK_GAMEPAD_A:                       "VK_GAMEPAD_A",
+	VK_GAMEPAD_B:                       "VK_GAMEPAD_B",
+	VK_GAMEPAD_X:                       "VK_GAMEPAD_X",
+	VK_GAMEPAD_Y:                       "VK_GAMEPAD_Y",
+	VK_GAMEPAD_RIGHT_SHOULDER:          "VK_GAMEPAD_RIGHT_SHOULDER",
+	VK_GAMEPAD_LEFT_SHOULDER:           "VK_GAMEPAD_LEFT_SHOULDER",
+	VK_GAMEPAD_LEFT_TRIGGER:            "VK_GAMEPAD_LEFT_TRIGGER",
+	VK_GAMEPAD_RIGHT_TRIGGER:           "VK_GAMEPAD_RIGHT_TRIGGER",
+	VK_GAMEPAD_DPAD_UP:                 "VK_GAMEPAD_DPAD_UP",
+	VK_GAMEPAD_DPAD_DOWN:               "VK_GAMEPAD_DPAD_DOWN",
+	VK_GAMEPAD_DPAD_LEFT:               "VK_GAMEPAD_DPAD_LEFT",
+	VK_GAMEPAD_DPAD_RIGHT:              "VK_GAMEPAD_DPAD_RIGHT",
+	VK_GAMEPAD_MENU:                    "VK_GAMEPAD_MENU",
+	VK_GAMEPAD_VIEW:                    "VK_GAMEPAD_VIEW",
+	VK_GAMEPAD_LEFT_THUMBSTICK_BUTTON:  "VK_GAMEPAD_LEFT_THUMBSTICK_BUTTON",
+	VK_GAMEPAD_RIGHT_THUMBSTICK_BUTTON: "VK_GAMEPAD_RIGHT_THUMBSTICK_BUTTON",
+	VK_GAMEPAD_LEFT_THUMBSTICK_UP:      "VK_GAMEPAD_LEFT_THUMBSTICK_UP",
+	VK_GAMEPAD_LEFT_THUMBSTICK_DOWN:    "VK_GAMEPAD_LEFT_THUMBSTICK_DOWN",
+	VK_GAMEPAD_LEFT_THUMBSTICK_RIGHT:   "VK_GAMEPAD_LEFT_THUMBSTICK_RIGHT",
+	VK_GAMEPAD_LEFT_THUMBSTICK_LEFT:    "VK_GAMEPAD_LEFT_THUMBSTICK_LEFT",
+	VK_GAMEPAD_RIGHT_THUMBSTICK_UP:     "VK_GAMEPAD_RIGHT_THUMBSTICK_UP",
+	VK_GAMEPAD_RIGHT_THUMBSTICK_DOWN:   "VK_GAMEPAD_RIGHT_THUMBSTICK_DOWN",
+	VK_GAMEPAD_RIGHT_THUMBSTICK_RIGHT:  "VK_GAMEPAD_RIGHT_THUMBSTICK_RIGHT",
+	VK_GAMEPAD_RIGHT_THUMBSTICK_LEFT:   "VK_GAMEPAD_RIGHT_THUMBSTICK_LEFT",
+	VK_OEM_4:                           "VK_OEM_4",
+	VK_OEM_5:                           "VK_OEM_5",
+	VK_OEM_6:                           "VK_OEM_6",
+	VK_OEM_7:                           "VK_OEM_7",
+	VK_OEM_8:                           "VK_OEM_8",
+	VK_OEM_102:                         "VK_OEM_102",
+	VK_PROCESSKEY:                      "VK_PROCESSKEY",
+	VK_PACKET:                          "VK_PACKET",
+	VK_ATTN:                            "VK_ATTN",
+	VK_CRSEL:                           "VK_CRSEL",
+	VK_EXSEL:                           "VK_EXSEL",
+	VK_EREOF:                           "VK_EREOF",
+	VK_PLAY:                            "VK_PLAY",
+	VK_ZOOM:                            "VK_ZOOM",
+	VK_NONAME:                          "VK_NONAME",
+	VK_PA1:                             "VK_PA1",
+	VK_OEM_CLEAR:                       "VK_OEM_CLEAR",
+}
+
 // See: https://learn.microsoft.com/en-us/windows/console/key-event-record-str
 type KEY_EVENT_RECORD struct {
 	KeyDown         uint32
 	RepeatCount     uint16
 	VirtualKeyCode  uint16
 	VirtualScanCode uint16
-	Char            [2]byte
+	Char            uint16
 	ControlKeyState uint32
 }
 
@@ -227,12 +474,27 @@ const (
 	RIGHTMOST_BUTTON_PRESSED     = 0x0002
 )
 
+var ButtonStateMap = map[uint32]string{
+	FROM_LEFT_1ST_BUTTON_PRESSED: "FROM_LEFT_1ST_BUTTON_PRESSED",
+	FROM_LEFT_2ND_BUTTON_PRESSED: "FROM_LEFT_2ND_BUTTON_PRESSED",
+	FROM_LEFT_3RD_BUTTON_PRESSED: "FROM_LEFT_3RD_BUTTON_PRESSED",
+	FROM_LEFT_4TH_BUTTON_PRESSED: "FROM_LEFT_4TH_BUTTON_PRESSED",
+	RIGHTMOST_BUTTON_PRESSED:     "RIGHTMOST_BUTTON_PRESSED",
+}
+
 const (
 	DOUBLE_CLICK   = 0x0002
 	MOUSE_HWHEELED = 0x0008
 	MOUSE_MOVED    = 0x0001
 	MOUSE_WHEELED  = 0x0004
 )
+
+var MouseEventFlagMap = map[uint32]string{
+	DOUBLE_CLICK:   "DOUBLE_CLICK",
+	MOUSE_HWHEELED: "MOUSE_HWHEELED",
+	MOUSE_MOVED:    "MOUSE_MOVED",
+	MOUSE_WHEELED:  "MOUSE_WHEELED",
+}
 
 // See: https://learn.microsoft.com/en-us/windows/console/mouse-event-record-str
 type MOUSE_EVENT_RECORD struct {
