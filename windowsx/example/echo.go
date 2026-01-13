4c81452349dce6016e5ba255432d1059c26713f6
Mateusz Gasior <mr.nimelo@gmail.com>
2026-01-13 19:40:30 +0000

    added echo example to the windowsx using utf16x

---
+++ windowsx/example/echo.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nimelo/tui-go/utf16x"
+	"github.com/nimelo/tui-go/windowsx"
+	"golang.org/x/sys/windows"
+	"golang.org/x/term"
+)
+
+func main() {
+	fd := os.Stdin.Fd()
+
+	state, err := term.MakeRaw(int(fd))
+	if err != nil {
+		panic(err)
+	}
+	restore := func() {
+		term.Restore(int(fd), state)
+	}
+	defer restore()
+
+	rs := utf16x.RuneScanner{}
+	buffer := make([]windowsx.INPUT_RECORD, 1)
+	for {
+		_, err := windowsx.ReadConsoleInput(windows.Handle(fd), buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		record := buffer[0]
+		if record.EventType == windowsx.KEY_EVENT {
+			keyEvent := record.KeyEvent()
+
+			if keyEvent.KeyDown == 0 {
+				continue
+			}
+
+			r, ok := rs.Write(keyEvent.Char)
+
+			if !ok {
+				continue
+			}
+
+			if r == '\x1b' {
+				break
+			}
+
+			fmt.Printf("%c", r)
+		}
+	}
+}
