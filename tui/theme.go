c50e18f4f436d27c0ab4ca4752927b41466be677
Mateusz Gasior <mr.nimelo@gmail.com>
2026-02-02 11:01:05 +0000

    consensus on theme

--- tui/theme.go
+++ tui/theme.go
@@ -1,19 +1,33 @@
 package tui
 
-// 2b2d42-8d99ae-edf2f4-ef233c-d90429
 type Theme struct {
-	Primary    Color
-	Secondary  Color
-	Background Color
-	Foreground Color
-	Accent     Color
-	Error      Color
-	Success    Color
+	Surface   Color
+	OnSurface Color
 
-	Text      Style
-	Border    Style
-	Focus     Style
-	Header    Style
-	Selection Style
-	Inactive  Style
+	Primary   Color
+	Secondary Color
+	Tertiary  Color
+
+	Accent Color
+	Danger Color
+
+	Outline        Color
+	OutlineVariant Color
+}
+
+func NewTheme() Theme {
+	return Theme{
+		Surface:   ColorHex(0x2B2D42),
+		OnSurface: ColorHex(0xEDF2F4),
+
+		Outline:        ColorHex(0x8D99AE),
+		OutlineVariant: ColorHex(0x8D99AE),
+
+		Primary:   ColorHex(0xEF233C),
+		Secondary: ColorHex(0xD90429),
+		Tertiary:  ColorHex(0xD90429),
+
+		Accent: ColorHex(0xEF233C),
+		Danger: ColorHex(0xD90429),
+	}
 }
