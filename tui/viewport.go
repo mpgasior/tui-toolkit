405e5c53a94d53765300f2c27b45340f423a3fb7
Mateusz Gasior <mr.nimelo@gmail.com>
2026-02-03 10:07:48 +0000

    viewport offset

--- tui/viewport.go
+++ tui/viewport.go
@@ -51,3 +51,35 @@ func (vp Viewport) Slice(x, y, w, h int) Viewport {
 		h:      max(0, min(h, remainingH)),
 	}
 }
+
+func (vp Viewport) Offset(offsets ...int) Viewport {
+	var top, right, bottom, left int
+
+	switch len(offsets) {
+	case 1:
+		top, right, bottom, left = offsets[0], offsets[0], offsets[0], offsets[0]
+	case 2:
+		top, bottom = offsets[0], offsets[0]
+		right, left = offsets[1], offsets[1]
+	case 3:
+		top = offsets[0]
+		right, left = offsets[1], offsets[1]
+		bottom = offsets[2]
+	case 4:
+		top, right, bottom, left = offsets[0], offsets[1], offsets[2], offsets[3]
+	}
+
+	newX := vp.x + left
+	newY := vp.y + top
+
+	newW := max(0, vp.w-left-right)
+	newH := max(0, vp.h-top-bottom)
+
+	return Viewport{
+		buffer: vp.buffer,
+		x:      newX,
+		y:      newY,
+		w:      newW,
+		h:      newH,
+	}
+}
